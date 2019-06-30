b4b997037cc42b86fdce1570ceffd3ff946774c4
Matteo Giachino <matteog@gmail.com>
2019-06-30 18:54:55 +0200

    uses a vector of matchers instead of an Option, for multiple queries

--- src/matcher.rs
+++ src/matcher.rs
@@ -21,51 +21,51 @@ impl<'a> Matcher<'a> {
     }
 }
 
-pub fn match_query<'a>(query: &str, matchers: &'a Vec<Matcher>) -> Option<&'a Matcher<'a>> {
+pub fn match_query<'a>(query: &str, matchers: &'a Vec<Matcher>) -> Vec<&'a Matcher<'a>> {
     let query_parsed = parse_query(query).expect("malformed query");
     query_parsed
         .definitions
         .into_iter()
-        .filter_map(|definition| match_definition(&definition, matchers))
-        .find(|m| true)
+        .flat_map(|definition| match_definition(&definition, matchers))
+        .collect()
 }
 
 fn match_definition<'a>(
     definition: &Definition,
     matchers: &'a Vec<Matcher <'a>>,
-) -> Option<&'a Matcher<'a>> {
+) -> Vec<&'a Matcher<'a>> {
     match definition {
         Operation(operation_definition) => {
             match_operation_definition(operation_definition, matchers)
         }
-        _ => None,
+        _ => Default::default(),
     }
 }
 
 fn match_operation_definition<'a>(
     operation_definition: &OperationDefinition,
     matchers: &'a Vec<Matcher<'a>>,
-) -> Option<&'a Matcher<'a>> {
+) -> Vec<&'a Matcher<'a>> {
     match operation_definition {
         OperationDefinition::SelectionSet(selection_set) => selection_set
             .items
             .iter()
-            .filter_map(|selection| match_selection(selection, matchers))
-            .find(|m| true)
+            .flat_map(|selection| match_selection(selection, matchers))
+            .collect()
         ,
-        OperationDefinition::Query(query) => None,
-        OperationDefinition::Mutation(_) => None,
-        OperationDefinition::Subscription(_) => None,
+        OperationDefinition::Query(query) => Default::default(),
+        OperationDefinition::Mutation(_) => Default::default(),
+        OperationDefinition::Subscription(_) => Default::default(),
     }
 }
 
 fn match_selection<'a>(
     selection: &Selection,
     matchers: &'a Vec<Matcher<'a>>,
-) -> Option<&'a Matcher<'a>> {
+) -> Vec<&'a Matcher<'a>> {
     match selection {
-        Selection::Field(field) => matchers.iter().find(|matcher| matcher.matches_field(field)),
-        Selection::FragmentSpread(_) => None,
-        Selection::InlineFragment(_) => None,
+        Selection::Field(field) => matchers.iter().filter(|matcher| matcher.matches_field(field)).collect(),
+        Selection::FragmentSpread(_) => Default::default(),
+        Selection::InlineFragment(_) => Default::default(),
     }
 }
--- tests/graphql_http_match.rs
+++ tests/graphql_http_match.rs
@@ -6,7 +6,7 @@ use graphql_mock_pal::matcher::{match_query, Matcher, MatcherOperation};
 fn test_no_match_if_no_matcher_is_specified() {
     let query = "{query_name {field1 field2}}";
     let matchers = vec![];
-    assert!(match_query(query, &matchers).is_none());
+    assert!(match_query(query, &matchers).is_empty());
 }
 
 #[test]
@@ -18,11 +18,11 @@ fn test_simple_match_with_one_matcher_on_the_query_name() {
     };
 
     assert_eq!(
-        &Matcher {
+        vec![&Matcher {
             operation: MatcherOperation::Query,
-            name: "query_name"
-        },
-        match_query(query, &vec![matcher]).unwrap()
+            name: "query_name",
+        }],
+        match_query(query, &vec![matcher])
     );
 }
 
@@ -33,9 +33,35 @@ fn test_non_matching_with_query_name() {
         operation: MatcherOperation::Query,
         name: "another_query_name",
     };
+    let expected:Vec<&Matcher> = vec![];
 
     assert_eq!(
-        None,
+        expected,
         match_query(query, &vec![matcher])
     );
 }
+
+#[test]
+fn test_two_different_queried() {
+    let query = "{query_name {field1 field2} query_2 { a b c }}";
+    let matcher = Matcher {
+        operation: MatcherOperation::Query,
+        name: "query_name",
+    };
+    let matcher2 = Matcher {
+        operation: MatcherOperation::Query,
+        name: "query_2",
+    };
+
+    assert_eq!(
+        vec![&Matcher {
+            operation: MatcherOperation::Query,
+            name: "query_name",
+        }, &Matcher {
+            operation: MatcherOperation::Query,
+            name: "query_2",
+        }],
+        match_query(query, &vec![matcher, matcher2])
+    );
+}
+
