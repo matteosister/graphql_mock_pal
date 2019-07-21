5145ec8a959621009328701074c92ea7d6a7a8c7
Matteo Giachino <matteog@gmail.com>
2019-07-21 22:49:04 +0200

    adds an actor for the config

---
+++ src/config.rs
@@ -0,0 +1,33 @@
+//use graphql_mock_pal::errors;
+//use graphql_mock_pal::errors::Result;
+use graphql_mock_pal::matcher::Matcher;
+//use snafu::ResultExt;
+//use std::path::Path;
+use std::sync::mpsc::{Sender, Receiver};
+use std::thread::sleep;
+use std::time::Duration;
+
+#[derive(Debug)]
+pub struct Config {
+    matchers: Vec<Matcher>,
+}
+
+pub fn start(tx: Sender<Config>, receiver : Receiver<String>) {
+    for msg in receiver {
+        println!("config received {}", msg);
+        let config = get_config();
+        tx.send(config);
+    }
+}
+
+fn get_config() -> Config {
+    Config { matchers: vec![] }
+}
+
+/*fn parse_file(path: &str) -> Result<()> {
+    let filename = Path::new(path);
+    std::fs::read(filename).context(errors::OpenConfig {
+        filename: filename.to_string_lossy().to_string(),
+    })?;
+    Ok(())
+}*/
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,33 @@
 pub mod application;
 pub mod cli;
+pub mod config;
 pub mod errors;
 pub mod matcher;
 pub mod state;
 
 use crate::application::query_handler;
-use crate::errors::*;
+use crate::config::start;
 use crate::state::AppState;
 use actix_web::{web, web::post, App, HttpServer};
 use cli::app;
-use snafu::{ResultExt, Snafu};
-use std::path::Path;
+use snafu::Snafu;
+use std::sync::mpsc;
+use std::thread;
 
 fn main() -> std::io::Result<()> {
     let matches = app().get_matches();
-    dbg!(matches.value_of("file"));
+    let (main_tx, main_rx) = mpsc::channel();
+    let (config_tx, config_rx) = mpsc::channel();
+
+    thread::spawn(move || start(main_tx, config_rx));
+
+    config_tx.send("dammi la config".to_owned()).unwrap();
+    let config = main_rx.recv().unwrap();
+    println!("{:?}", config);
+
+    config_tx.send("dammi la config".to_owned()).unwrap();
+    let config = main_rx.recv().unwrap();
+    println!("{:?}", config);
 
     HttpServer::new(|| {
         App::new()
@@ -29,10 +42,4 @@ fn main() -> std::io::Result<()> {
 pub enum Error {
     #[snafu(display("Could not open config file: {}", source))]
     OpenConfig { source: std::io::Error },
-}
-
-fn parse_file() -> Result<()> {
-    let filename = Path::new("mocks.json");
-    std::fs::read(filename).context(errors::OpenConfig {filename: filename.to_string_lossy().to_string()})?;
-    Ok(())
 }
